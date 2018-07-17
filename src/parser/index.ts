6317ec16770b42625e2eb52c2c4d07a51d802b2e
Peter East <peter@repositive.io>
2018-07-17 20:08:23 +0100

    --wip--

---
+++ src/parser/index.ts
@@ -0,0 +1,16 @@
+// Code to ingest the events based on definitions, and analyse the entities and the relationships between them
+import * as yaml from 'js-yaml';
+
+const definitions_location = "./events.yaml";
+
+const read_events = (loc: string): any => {
+  return '';
+};
+
+export default function() {
+  // Read the events location from somewhere
+  // Find all the entity's attributes
+  // Check the logic of the events
+  // Create a dependency tree for all the attributes
+  // Something
+}
