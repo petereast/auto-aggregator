9279df2814347b6946f94e7fd51877213e805442
Peter East <peter@repositive.io>
2018-07-20 19:35:45 +0100

    [F] Finish some boilerplate, still probably won't compile until I merge...

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,16 +1,29 @@
 // Code to ingest the events based on definitions, and analyse the entities and the relationships between them
 import * as yaml from 'js-yaml';
-
+import * as fs from 'fs';
 const definitions_location = "./events.yaml";
 
-const read_events = (loc: string): any => {
-  return '';
+const read_events = (loc: string = "./events.yaml"): any => {
+  const raw_events = yaml.safeLoad(fs.readFileSync('./events.yaml', 'utf-8'));
+  // Maybe do some more stuff here?
+  // Possibly validate data_structure
+  if (validate(raw_events)) {
+    return raw_events.defs;
+  } else {
+    return undefined;
+  }
 };
 
-export default function() {
-  // Read the events location from somewhere
-  // Find all the entity's attributes
-  // Check the logic of the events
-  // Create a dependency tree for all the attributes
-  // Something
-}
+const parse_query = (q: string) => {
+  // return a query object
+  // q takes the form
+  // SELECT {attributes} WHERE [{condition}]
+};
+
+const validate = (defs: any): boolean => {
+  return true;
+};
+
+export {
+  read_events,
+};
