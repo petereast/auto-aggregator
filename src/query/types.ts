cb14b0e4d9c36243a4648bc620732008fe1f9688
Peter East <peter@repositive.io>
2018-08-22 19:06:53 +0100

    Refactor type definitions

---
+++ src/query/types.ts
@@ -0,0 +1,42 @@
+// Type definitions for the query builder
+export interface Query {
+  _select_terms: string[];
+  _conditions: any;
+
+  select: (term: string | string[]) => Query;
+  where: () => any; // Returns condition chain builder
+
+  _set_condition: (cond: any) => any;
+}
+
+export interface QueryBuilder {
+  create: () => Query;
+  query: Query;
+}
+
+export interface ConditionChainBuilder {
+  create: (_parent: Query) => ConditionChain;
+  condition_chain: ConditionChain;
+}
+
+export interface ConditionChain {
+  parent: Query | undefined;
+  key: (item: string) => Condition;
+  and: () => null;
+}
+
+export interface ConditionBuilder {
+  condition: Condition;
+  create: (_parent: Query, key: string) => Condition;
+}
+
+export interface Condition {
+  parent: Query | undefined;
+  // All evaluations should return a query
+  key: string;
+  operation: 'eq' | 'neq' | undefined;
+  value: any;
+
+  equal: (value: any) => Query;
+  not_equal: (value: any) => Query;
+}
