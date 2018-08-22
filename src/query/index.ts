0d53a4beb2fc6679e5b6fde41ae2711a559ea20a
Peter East <peter@repositive.io>
2018-08-22 17:33:30 +0100

    Add a basic unit test to query builder

--- src/query/index.ts
+++ src/query/index.ts
@@ -1,11 +1,15 @@
 // Query object
 // The idea is to have a composable 'query builder' that's functional and lovely
+import * as R from 'ramda';
 
 interface Query {
   _select_terms: string[];
+  _conditions: any;
 
-  select: (term: string) => Query;
+  select: (term: string | string[]) => Query;
   where: () => any; // Returns condition chain builder
+
+  _set_condition: (cond: any) => any;
 }
 
 export interface QueryBuilder {
@@ -21,14 +25,30 @@ export const query_builder: QueryBuilder = {
   query: {
 
     _select_terms: [],
+    _conditions: {},
 
-    select(term: string) {
-      this._select_terms.push(term);
-      return this;
+    select(term: string | string[]) {
+      return R.set(
+        R.lensProp("_select_terms"),
+        R.pipe(
+          R.append(term),
+          R.flatten)(this._select_terms),
+        this);
     },
-    where() {
-      console.log("where");
-      return condition_chain_builder.create(this);
+    where(conditions?: any) {
+      if (conditions) {
+        return this._select_conditions(conditions);
+      } else {
+        console.log("where");
+        return condition_chain_builder.create(this);
+      }
+    },
+    _set_condition(cond: Condition) {
+      return R.set(
+        R.lensPath(['_conditions', cond.operation as any, cond.key]),
+        cond.value,
+        this,
+      );
     },
   },
 };
@@ -55,7 +75,7 @@ const condition_chain_builder: ConditionChainBuilder = {
     parent: undefined,
     key(item: string) {
       console.log(item);
-      return ConditionBuilder.create(this.parent);
+      return ConditionBuilder.create(this.parent, item);
     },
     and() {
       return null;
@@ -65,32 +85,52 @@ const condition_chain_builder: ConditionChainBuilder = {
 
 interface ConditionBuilder {
   condition: Condition;
-  create: (_parent: Query) => Condition;
+  create: (_parent: Query, key: string) => Condition;
 }
 
 interface Condition {
-  parent: any;
+  parent: Query | undefined;
   // All evaluations should return a query
+  key: string;
+  operation: 'eq' | 'neq' | undefined;
+  value: any;
+
   equal: (value: any) => Query;
   not_equal: (value: any) => Query;
 }
 
 const ConditionBuilder: ConditionBuilder = {
-  create(_parent: any) {
+  create(_parent: any, _key: string) {
     const tmp = Object.create(this.condition);
     tmp.parent = _parent;
+    tmp.key = _key;
     return tmp;
   },
 
   condition: {
-    parent: null,
+    parent: undefined,
+    key: "",
+    operation: undefined,
+    value: undefined,
+
     equal(value: any) {
-      console.log("EQ:", value);
-      return this.parent;
+      return this.parent._set_condition(
+        {
+          ...this,
+          operation: 'eq',
+          value,
+        },
+      );
     },
+
     not_equal(value: any) {
-      console.log("NEQ:", value);
-      return this.parent;
+      return this.parent._set_condition(
+        {
+          ...this,
+          operation: 'neq',
+          value,
+        },
+      );
     },
   },
 };
